2995d76ba4adddeb60c734af711da3d755a54795
geocodefarm <support@geocode.farm>
2025-04-04 23:01:44 +0200

    Create installed.php

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'geocodefarm/geocodefarm',
+        'pretty_version' => '4.0',
+        'version' => '4.0.0.0',
+        'reference' => null,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'geocodefarm/geocodefarm' => array(
+            'pretty_version' => '4.0',
+            'version' => '4.0.0.0',
+            'reference' => null,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
